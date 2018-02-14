a41269b92e40831a845a3f059ba88cec9d941a14
Saleem Rashid <dev@saleemrashid.com>
2018-02-14 16:08:13 +0000

    includes: Add MPU

--- includes.h
+++ includes.h
@@ -1,9 +1,10 @@
 #define STM32F2
 
-#include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/cm3/mpu.h>
 #include <libopencm3/stm32/desig.h>
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/memorymap.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/rng.h>
 #include <libopencm3/stm32/spi.h>
